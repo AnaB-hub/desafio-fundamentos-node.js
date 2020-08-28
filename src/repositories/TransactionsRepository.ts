e1fae464e8c72a5d7ac5734ddb2989e485903e93
Ana Beatriz Garcia <ana-bia1369@hotmail.com>
2020-08-28 13:27:34 -0300

    Resolução do desafio: Primeiro projeto Node.js
    Link: https://github.com/rocketseat-education/bootcamp-gostack-desafios/tree/master/desafio-fundamentos-nodejs

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,61 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
+
+  private balance: Balance = {
+    income: 0,
+    outcome: 0,
+    total: 0,
+  };
 
   constructor() {
     this.transactions = [];
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const { income, outcome } = this.transactions.reduce(
+      (total: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            total.income += transaction.value;
+            break;
+          case 'outcome':
+            total.outcome += transaction.value;
+            break;
+          default:
+            break;
+        }
+
+        return total;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const total = income - outcome;
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
